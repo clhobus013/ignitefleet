a7a9d6d29208f5dbbfd489d40a2abf83e3bd4719
Claudia Hobus <clauu013@gmail.com>
2025-04-13 16:16:09 -0300

    Implementação login com google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,53 @@
+import { useState } from 'react';
+import { Alert } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from '@env';
 import { Container, Slogan, Title } from './styles';
 import backgroundImg from '../../assets/background.png';
+
 import { Button } from '../../components/Button';
 
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID
+});
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function handleGoogleSignIn() { 
+    try {
+      setIsAuthenticating(true);
+
+      const { data } = await GoogleSignin.signIn();
+
+      if(data?.idToken) {
+
+      } else {
+        Alert.alert("Entrar", "Não foi possível conectar-se a sua conta google.");
+        setIsAuthenticating(false);
+      }
+      
+    } catch (error) {
+      console.log(error);
+      setIsAuthenticating(false);
+      Alert.alert("Entrar", "Não foi possível conectar-se a sua conta google.")
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title='Entrar com o Google'/>
+      <Button
+        title='Entrar com o Google'
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
+      />
     </Container>
   );
 }
